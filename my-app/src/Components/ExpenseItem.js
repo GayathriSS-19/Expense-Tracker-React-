9e63699632c1fdbcf5223d72b6e528ee300e21e2
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-10-25 15:59:06 +0530

    Added Card Component
    To use Nested Components concept

--- my-app/src/Components/ExpenseItem.js
+++ my-app/src/Components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import './ExpenseItem.css'
 import React from 'react'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 export default function ExpenseItem(props) {
     // let title=props.title;
@@ -8,19 +9,20 @@ export default function ExpenseItem(props) {
     // const year=props.date.getFullYear()
     // const day=props.date.toLocaleString('en-US',{day:'2-digit'})
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       {/* <div>{props.date}</div>  we are getting date obj so can't do like this*/ }
       {/* <div>
         <div>{month}</div>
         <div>{year}</div>
         <div>{day}</div>
       </div> */}
+ 
       <ExpenseDate date = {props.date}/>
       <div className='expense-item__description'>
         <h2>{ props.title }</h2>
         { /* <h2>title</h2> */}
         <div className='expense-item__price'>${ props.amount }</div>
       </div>
-    </div>
+    </Card>
   )
 }
