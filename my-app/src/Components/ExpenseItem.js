2f0aea9db84956a41782dbd804a6d4efcdce2a0d
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-10-25 15:27:18 +0530

    Made multi-component page
    Made date as yet another project

--- my-app/src/Components/ExpenseItem.js
+++ my-app/src/Components/ExpenseItem.js
@@ -1,19 +1,21 @@
 import './ExpenseItem.css'
 import React from 'react'
+import ExpenseDate from './ExpenseDate'
 
 export default function ExpenseItem(props) {
     // let title=props.title;
-    const month=props.date.toLocaleString('en-US',{month:'long'})
-    const year=props.date.getFullYear()
-    const day=props.date.toLocaleString('en-US',{day:'2-digit'})
+    // const month=props.date.toLocaleString('en-US',{month:'long'})
+    // const year=props.date.getFullYear()
+    // const day=props.date.toLocaleString('en-US',{day:'2-digit'})
   return (
     <div className="expense-item">
       {/* <div>{props.date}</div>  we are getting date obj so can't do like this*/ }
-      <div>
+      {/* <div>
         <div>{month}</div>
         <div>{year}</div>
         <div>{day}</div>
-      </div>
+      </div> */}
+      <ExpenseDate date = {props.date}/>
       <div className='expense-item__description'>
         <h2>{ props.title }</h2>
         { /* <h2>title</h2> */}
