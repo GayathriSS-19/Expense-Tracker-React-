4fa650c6b5b77588b9820fd9d271e5817d7a3bf8
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-10-27 22:01:06 +0530

    Added States for form inputs and two-way Binding done via states

--- my-app/src/Components/NewExpenses/Expenseform.js
+++ my-app/src/Components/NewExpenses/Expenseform.js
@@ -1,27 +1,55 @@
-import React from 'react'
+import React,{useState} from 'react'
+import ExpenseItem from '../Expenses/ExpenseItem'
 import "./Expenseform.css"
 
+
 function Expenseform() {
+    const [enteredTitle,setEnteredTitle] =useState("")
+    const [enteredAmount,setEnteredAmount] =useState("")
+    const [enteredDate,setEnteredDate] =useState("")
+    const titleChangeHandler = (event) =>{
+        //  console.log(event.target.value)
+        setEnteredTitle(event.target.value)
+    }
+    const amountChangeHandler = (event) =>{
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) =>{
+        setEnteredDate(event.target.value)
+    }
+    const submitHandler=(event) =>{
+        event.preventDefault();
+
+        const expenseData={         //Two-way Binding
+            title:enteredTitle,
+            amount:enteredAmount,
+            date:enteredDate
+        }
+        console.log(expenseData)
+        setEnteredAmount("")
+        setEnteredDate("")
+        setEnteredTitle("")      //Two-way Binding
+    }
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
             <label>Title</label>
-            <input type="text"/>
+            <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
         </div>
         <div className='new-expense__control'>
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01"/>
+            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount}/>
         </div>
         <div className='new-expense__control'>
             <label>Date</label>
-            <input type="date"/>
+            <input type="date" onChange={dateChangeHandler} value={enteredDate}/>
         </div>
-
       </div>
       <div className='new-expense__actions'>
         <button type='submit'>Add the Expense</button>
       </div>
+      {/* <ExpenseItem date={enteredDate} amount={enteredAmount} title={enteredTitle}/> */}
     </form>
   )
 }
