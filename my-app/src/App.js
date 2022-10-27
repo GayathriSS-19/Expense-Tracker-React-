23664997f5c0a31a87c2571b4e6ca38247b32b0b
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-10-27 21:32:57 +0530

    Added a form for adding newExpense

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ExpenseItem from './Components/Expenses/ExpenseItem'
 import Expenses from './Components/Expenses/Expenses'
+import NewExpense from './Components/NewExpenses/NewExpense'
 
  const App=() => {      //used arrow function
   let expenses=[
@@ -35,7 +36,9 @@ import Expenses from './Components/Expenses/Expenses'
       <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount}/>
       <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount}/>
       <ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount}/> */}
+      <NewExpense/>
       <Expenses item={expenses}/>
+
     </div>
   )
 }
